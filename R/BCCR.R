f1fb67329652f410d341592e46ab9b37ebca0c6a
rafaelcb <mail@rafaelcb.com>
2016-08-28 22:04:35 -0600

    Added functionality for multiple series

--- R/BCCR.R
+++ R/BCCR.R
@@ -1,33 +1,49 @@
-getDataBCCR <- function(indicator, start="11/02/1989", end = "today", 
+#R version 3.3.1 (Bug in Your Hair)
+library(RCurl)
+library(XML)
+
+getDataBCCR <- function(indicator, start="11/02/1989", end = "today",
                         name= "me", sublevels = "N"){
-        
+
         if(end == "today") end = strftime(Sys.time(),"%d/%m/%Y")
-        
-        baseSource <- paste("http://indicadoreseconomicos.bccr.fi.cr/",
-                "indicadoreseconomicos/WebServices/",
+
+
+        final <- data.frame(Indicador = character(),
+                            Fecha = as.Date(character()),
+                                  Value = numeric(),
+                                  stringsAsFactors=FALSE)
+
+
+        for(ind in indicator){
+                baseSource <- paste("http://indicadoreseconomicos.bccr.fi.cr/",
+                        "indicadoreseconomicos/WebServices/",
                 "wsIndicadoresEconomicos.asmx/ObtenerIndicadoresEconomicosXML",
-                sep = "")
-        
-        htmlRequest <- getForm(baseSource, tcIndicador = indicator, 
-                tcFechaInicio = start, tcFechaFinal = end, tcNombre = name, 
-                tnSubNiveles = sublevels)
-        
-        #Get rid of <string> tag that messes up xmlTree
-        html <- htmlParse(htmlRequest, asText = TRUE)
-        temp <- xpathSApply(html, "//string", xmlValue)
-        
-        xml <- xmlTreeParse(temp)
-        root <- xmlRoot(xml)
-        data <- xmlSApply(root, function(x) xmlSApply(x, xmlValue))
-        
-        #Format data
-        data <- t(data)
-        rownames(data) <- NULL
-        data <- as.data.frame(data, stringsAsFactors = FALSE)
-        colnames(data) <- c("Indicador", "Fecha", "Valor")
-        data$Valor = as.numeric(data$Valor)
-        data$Fecha <- as.Date(data$Fecha, "%Y-%m-%d")
-        
-        return(data)
-        
+                                    sep = "")
+
+                htmlRequest <- getForm(baseSource, tcIndicador = ind,
+                        tcFechaInicio = start, tcFechaFinal = end,
+                        tcNombre = name,tnSubNiveles = sublevels)
+
+                #Get rid of <string> tag that messes up xmlTree
+                html <- htmlParse(htmlRequest, asText = TRUE)
+                temp <- xpathSApply(html, "//string", xmlValue)
+
+                xml <- xmlTreeParse(temp)
+                root <- xmlRoot(xml)
+                temp <- xmlSApply(root, function(x) xmlSApply(x, xmlValue))
+
+                #Format temp
+                temp <- t(temp)
+                rownames(temp) <- NULL
+                temp <- as.data.frame(temp, stringsAsFactors = FALSE)
+                colnames(temp) <- c("Indicador", "Fecha", "Valor")
+                temp$Valor = as.numeric(temp$Valor)
+                temp$Fecha <- as.Date(temp$Fecha, "%Y-%m-%d")
+                final <- rbind(final, temp)
+
+        }
+
+
+        return(final)
+
 }
